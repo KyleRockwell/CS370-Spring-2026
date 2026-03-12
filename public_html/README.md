057b81e41309b7f41eb51ea66c6bdd4700ce0ecc
Oellefsen321 <ellefseno@sonoma.edu>
2026-03-12 11:39:44 -0700

    Added addInfo.c, adds info to database with command line options, also updated README in public_html -OE

--- public_html/README.md
+++ public_html/README.md
@@ -1,3 +1,12 @@
 Basic functionality for login page. The only available username and password are "Username" and "Password" respectively.
 Feel free to make any changes you see fit.
-This is only on localhost for now (just for testing)
+
+3/12/26:
+    Added addInfo.c, which takes input for username and password as command line arguments and adds them to the database. It's not connected to the html files for now, but the functionality is there
+    It's not easy to run and I don't know how to do it on blue. On my local machine the command to compile it is:
+        
+        gcc addInfo.c -I/opt/homebrew/Cellar/mysql/9.6.0_1/include/ -L/opt/homebrew/Cellar/mysql/9.6.0_1/lib -lmysqlclient -o output
+
+    To get the first 2 options, I ran the command mysql_config --include and mysql_config --libs respecitvely.
+    after you compile the program it's pretty easy to run, just ./output username password
+    
---
+++ public_html/addInfo.c
@@ -0,0 +1,73 @@
+#include <mysql/mysql.h>
+#include <mysql/mysqld_error.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MYSQL_USERNAME "teamg_user"
+#define MYSQL_PASSWORD "LemonGreenApple"
+#define MYSQL_DATABASE "teamg_db"
+
+int main (int agrc, char *argv[])
+{
+  //Base length of the string to store input command, used later
+  int BASE_LENGTH = 76;
+
+  MYSQL *conn;
+  MYSQL_RES *res;
+  MYSQL_ROW row;
+
+  conn = mysql_init (NULL);
+// ***********************
+// * Connect to database *
+// ***********************
+  if (!mysql_real_connect(conn, "localhost", MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DATABASE, 0, NULL, 0))
+  {
+    fprintf (stderr, "%s\n", mysql_error(conn));
+    return (0);
+  }
+// ****************
+// * Send a query *
+// ****************
+  if (mysql_query(conn, "SELECT now()")) 
+  {
+    fprintf (stderr, "%s\n", mysql_error(conn));
+    return (0);
+  }
+
+// ******************************
+// * Retrieve result from query *
+// ******************************
+  res = mysql_use_result (conn);
+  while ((row = mysql_fetch_row(res)) != NULL)
+  {
+    printf("%s\n", row[0]);
+  }
+  mysql_free_result (res);
+
+  //Read comand line args into variables  
+  char *username = argv[1];
+  char *password = argv[2];
+
+  //Calculate the total length of the string that stores the command
+  int len1 = strlen(username);
+  int len2 = strlen(password);
+  int totalLen = BASE_LENGTH + len1 + len2;
+  char *cmd = malloc(totalLen);
+
+  //Concatenate the commmand into cmd
+  strcat(cmd, "INSERT INTO Users (Username, Password) VALUES (");
+  strcat(cmd, "'"); 
+  strcat(cmd, username);
+  strcat(cmd, "'");
+  strcat(cmd, ", ");
+  strcat(cmd, "'");
+  strcat(cmd, password);
+  strcat(cmd, "'");
+  strcat(cmd, ");");
+
+  //Execute the command
+  if( mysql_query(conn, cmd)) 
+	printf("Good!");
+
+  mysql_close (conn);
+}
